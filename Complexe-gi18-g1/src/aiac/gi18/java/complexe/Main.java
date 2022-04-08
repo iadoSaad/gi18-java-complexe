2fe15b2176da6b32da9a7532f4ca3b2ec32d4978
mac <mac@controller>
2022-04-08 09:41:15 +0000

    Test List Complexes

--- Complexe-gi18-g1/src/aiac/gi18/java/complexe/Main.java
+++ Complexe-gi18-g1/src/aiac/gi18/java/complexe/Main.java
@@ -1,34 +1,32 @@
 package aiac.gi18.java.complexe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Complexe c1=new Complexe(3, 1);
-		Complexe c2=new Complexe(1, -1);
-		Complexe c3=new Complexe(1,-1);
-		
-	
-		
-		
-		System.out.println(c1);
-		System.out.println(c2);
-		
-		System.out.println(c1.add(c2));
-		System.out.println(c1.sub(c2));
-		System.out.println(c1.add(1,2));
-		
-		System.out.println(Complexe.createdComplexe());
-		
-		List<Complexe> l=new ListComplexe();
-		c1.equals(c3);  c1.equals("hekks");
-		l.add(c1); l.add(c2); l.add(c1); l.add(c3);
-		System.out.println(l);
+		testListComplexe();
 		
 
+	}
+	
+	public static void testListComplexe(){
+		Complexe c1=new Complexe(3, 1);
+		Complexe c2=new Complexe(10, -1);
+		Complexe c3=new Complexe(1,-1);
+		
+		ListComplexe myList=new ListComplexe();
+		myList.add(c1);
+		myList.add(c2);
+		myList.add(c3);
+		
+		System.out.println(myList);
+		
+		Collections.sort(myList);
+		
+		System.out.println(myList);
+		
 	}
 
 }
